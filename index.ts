91d72badcb2ecd8f3da4dfc2819c7ee6b7e0598a
dxlliv <hello@dxlliv.dev>
2025-04-22 01:59:34 +0200

    Implement @nuxt/content

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import {defineNuxtModule, createResolver, addComponentsDir, extendPages, addImportsDir, installModule} from '@nuxt/kit'
 import {registerTailwindPath} from "@owdproject/core/runtime/utils/utilsApp";
+import { cpSync, existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
 
 const {resolve} = createResolver(import.meta.url);
 
@@ -43,13 +45,43 @@ export default defineNuxtModule({
             // add pages
 
             extendPages((pages) => {
-                // Add /test page
                 pages.push({
                     name: 'Docs',
                     path: '/docs',
                     file: resolve('./runtime/pages/docs/index.vue'),
                 })
+
+                pages.push({
+                    name: 'Page',
+                    path: '/docs/:slug',
+                    file: resolve('./runtime/pages/docs/[slug].vue'),
+                })
             })
+
+        }
+
+        {
+
+            // add content
+
+            const sourceContentDir = resolve('./runtime/content');
+            const targetContentDir = join(nuxt.options.rootDir, 'content');
+
+            nuxt.options.build.transpile.push('@nuxt/content');
+
+            await installModule('@nuxt/content');
+            await installModule('nuxt-toc');
+
+            try {
+                if (!existsSync(targetContentDir)) {
+                    mkdirSync(targetContentDir, { recursive: true });
+                }
+
+                cpSync(sourceContentDir, targetContentDir, { recursive: true });
+                console.log('[OWD] Copied content to project root');
+            } catch (err) {
+                console.error('[OWD] Failed to copy content to root', err);
+            }
 
         }
 
